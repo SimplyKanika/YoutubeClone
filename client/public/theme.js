d0c69f1634a6ad9405f9b9e38a12d99b48470e65
SimplyKanika <kanikabhuvad9@gmail.com>
2024-04-04 19:54:44 +0530

    deleted twilio  code

--- client/public/theme.js
+++ client/public/theme.js
@@ -21,7 +21,7 @@ document.body.style.backgroundColor = getThemeColor(theme);
 
 function getThemeColor(theme) {
     const themeColors = {
-        'morning': '#343a40', // Morning color #343a40
+        'morning': '#014D4E', // Morning color #343a40
         'afternoon': '#323131', // Afternoon color #323131
         'evening': '#152238', // Evening color #152238
         'night': '#001524', // Night color #001524
