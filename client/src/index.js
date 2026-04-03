1a8900e4ea238c657a015e09943bcacd67b4e394
SimplyKanika <kanikabhuvad9@gmail.com>
2026-04-03 16:39:39 +0530

    updated versions and resolved deployment issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
-import { applyMiddleware , compose } from 'redux';
-import { createStore } from 'redux'
-import {thunk} from "redux-thunk"
-import Reducers from "./Reducers"
+import { applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
+import { thunk } from "redux-thunk";
+import Reducers from "./Reducers";
 
-const store=createStore(Reducers,compose(applyMiddleware(thunk)))
+const store = createStore(Reducers, compose(applyMiddleware(thunk)));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
